65a745bfe06c07bbcb44f751cb65f40be96a87b8
jayeonKoo <137jayeon0322@naver.com>
2025-01-29 00:02:42 +0900

    feat/#50 auth,user,onboarding(#50)

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -12,6 +12,18 @@ import UserPage from '@/pages/user/page';
 import OnboardingPage from '@/pages/onboarding';
 import PhoneNumberPage from '@/pages/auth/phone-number';
 import CertifyPage from '@/pages/auth/certify';
+import GenderSelectionPage from '@/pages/auth/gender';
+import PersonalDataPage from '@/pages/auth/personal-data';
+import TosPage from '@/pages/auth/tos';
+import PrivacyPage from '@/pages/auth/privacy';
+import TermsLocationPage from '@/pages/auth/terms-location';
+import CompletePage from '@/pages/auth/complete';
+import EditPage from '@/pages/user/edit';
+import NoticePage from '@/pages/user/notice';
+import RequestPage from '@/pages/user/request';
+import TouPage from '@/pages/user/tou';
+import ManagePage from '@/pages/user/manage';
+import ResignPage from '@/pages/user/resign';
 
 const ProtectedPage = ({ children }: { children: React.ReactNode }) => {
   // 로그인 여부 확인해 페이지 보호 필요
@@ -30,7 +42,7 @@ const router = createBrowserRouter(
       // errorElement: <NotFoundPage />,
       children: [
         {
-          path: '/onboading',
+          index: true,
           element: <OnboardingPage />,
         },
         {
@@ -40,6 +52,30 @@ const router = createBrowserRouter(
         {
           path: '/auth/certify',
           element: <CertifyPage />,
+        },
+        {
+          path: '/auth/gender',
+          element: <GenderSelectionPage />,
+        },
+        {
+          path: '/auth/personal-data',
+          element: <PersonalDataPage />,
+        },
+        {
+          path: '/auth/tos',
+          element: <TosPage />,
+        },
+        {
+          path: '/auth/privacy',
+          element: <PrivacyPage />,
+        },
+        {
+          path: '/auth/terms-location',
+          element: <TermsLocationPage />,
+        },
+        {
+          path: '/auth/complete',
+          element: <CompletePage />,
         },
         {
           path: '/event',
@@ -76,6 +112,30 @@ const router = createBrowserRouter(
         {
           path: '/user',
           element: <UserPage />,
+        },
+        {
+          path: '/user/edit',
+          element: <EditPage />,
+        },
+        {
+          path: '/auth/notice',
+          element: <NoticePage />,
+        },
+        {
+          path: '/auth/request',
+          element: <RequestPage />,
+        },
+        {
+          path: '/auth/tou',
+          element: <TouPage />,
+        },
+        {
+          path: '/auth/manage',
+          element: <ManagePage />,
+        },
+        {
+          path: '/auth/resign',
+          element: <ResignPage />,
         },
       ],
     },
