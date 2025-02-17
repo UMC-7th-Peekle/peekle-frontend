b5c8778340b30eda5ef348344f4e4532a0f9343c
JOY <92291790+duwlsssss@users.noreply.github.com>
2025-02-18 01:27:10 +0900

    [✨ Feature] 이벤트 api 연결 (#90)
    * refactor: 맵 마커 표시 수정 (#80)
    
    * feat: 지도 영역 이벤트 없을때 이동 구현 (#80)
    
    * feat: 말풍선 svg 사용 (#80)
    
    * feat: 주기적으로 latestPos 업데이트 (#80)
    
    * fix: useSearchParams 사용 (#80)
    
    * style: svg 크기 변경 (#80)
    
    * feat: 지도 접근시 자기 위치 반영 (#80)
    
    * fix: merge 충돌 해결(#80)
    
    * test: api 연동 테스트 (#80)
    
    * feat: 이벤트 좋아요 낙관적 업데이트 로 지기 추가 (#80)
    
    * refactor: 이벤트 디테일 스켈레톤 보완 (#80)
    
    * refactor: 이벤트 스크랩 낙관적 업데이트 수정 (#80)
    
    * refactor: 이벤트 이미지 슬라이더 드래그 보완 (#80)
    
    * refactor: bottomSheet framer-motion 사용 (#80)
    
    * feat: 맵 검색 bottomSheet 추가 (#80)
    
    * feat: 맵 이벤트에서 지도 보기 버튼 추가 (#80)
    
    * feat: admin 레이아웃 추가 (#80)
    
    * fix:  로그인 오류 해결 (#80)
    
    * fix: 로컬 로그인 토큰 저장 (#80)
    
    * feat: 관리자 권한 탭 생성 (#80)
    
    * feat: 유효성 검사 포함된 input 제작 (#80)

---
+++ src/components/event/search-header/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 16px;
+`;
